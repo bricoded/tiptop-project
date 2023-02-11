39f9c5d4626fc02bd89ea16243efae9a3d303e7a
bricoded <bacarybricoded@gmail.com>
2023-02-11 11:05:10 +0100

    finished FAQs

---
+++ app.js
@@ -0,0 +1,49 @@
+let hiddenPara = document.querySelector('section#FAQs .btn-holder button div + p');
+let plus = document.querySelector('section#FAQs .btn-holder button div i');
+let colorChange = document.querySelector('section#FAQs .btn-holder button div p');
+
+function myShow(){
+    hiddenPara.classList.toggle('show');
+    plus.classList.toggle('rotate');
+    colorChange.classList.toggle('color');
+}
+
+let hiddenPara2 = document.querySelector('section#FAQs .btn-holder button:nth-child(2) div + p');
+let plus2 = document.querySelector('section#FAQs .btn-holder button:nth-child(2) div i');
+let colorChange2 = document.querySelector('section#FAQs .btn-holder button:nth-child(2) div p');2
+
+function myShow2(){
+    hiddenPara2.classList.toggle('show');
+    plus2.classList.toggle('rotate');
+    colorChange2.classList.toggle('color');
+}
+
+let hiddenPara3 = document.querySelector('section#FAQs .btn-holder button:nth-child(3) div + p');
+let plus3 = document.querySelector('section#FAQs .btn-holder button:nth-child(3) div i');
+let colorChange3 = document.querySelector('section#FAQs .btn-holder button:nth-child(3) div p');2
+
+function myShow3(){
+    hiddenPara3.classList.toggle('show');
+    plus3.classList.toggle('rotate');
+    colorChange3.classList.toggle('color');
+}
+
+let hiddenPara4 = document.querySelector('section#FAQs .btn-holder button:nth-child(4) div + p');
+let plus4 = document.querySelector('section#FAQs .btn-holder button:nth-child(4) div i');
+let colorChange4 = document.querySelector('section#FAQs .btn-holder button:nth-child(4) div p');2
+
+function myShow4(){
+    hiddenPara4.classList.toggle('show');
+    plus4.classList.toggle('rotate');
+    colorChange4.classList.toggle('color');
+}
+
+let hiddenPara5 = document.querySelector('section#FAQs .btn-holder button:nth-child(5) div + p');
+let plus5 = document.querySelector('section#FAQs .btn-holder button:nth-child(5) div i');
+let colorChange5 = document.querySelector('section#FAQs .btn-holder button:nth-child(5) div p');
+
+function myShow5(){
+    hiddenPara5.classList.toggle('show');
+    plus5.classList.toggle('rotate');
+    colorChange5.classList.toggle('color');
+}
